6d961932c386688e411d965a3a63969afd84ff18
Narongrit Rodphroboon <joker10130@gmail.com>
2019-08-17 12:07:30 +0700

    add 3 database data in models.py and add templates folder in settings.py

--- backend/smart_bin/settings.py
+++ backend/smart_bin/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'smart_bin.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['web_app/templates/'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- backend/web_app/models.py
+++ backend/web_app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class GarbageType(models.Model):
+    garbageId = models.IntegerField(primary_key=True)
+    garbageName = models.CharField(max_length=255)
+
+class Bin(models.Model):
+    binId = models.IntegerField(primary_key=True)
+
+class Contain(models.Model):
+    garbageId = models.ForeignKey(GarbageType, on_delete=models.CASCADE)
+    binId = models.ForeignKey(Bin, on_delete=models.CASCADE)
+    amount = models.IntegerField()
+    Level = models.IntegerField()
+

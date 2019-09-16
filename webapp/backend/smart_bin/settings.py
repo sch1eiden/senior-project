a312535855de6b53ff70adb29054638abf32554f
Narongrit Rodphroboon <joker10130@gmail.com>
2019-09-16 10:53:57 +0700

    collectstatic and change api to localhost:8000

--- webapp/backend/smart_bin/settings.py
+++ webapp/backend/smart_bin/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'smart_bin.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['Templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,7 +121,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATIC_URL = '/static/'
 
 REST_FRAMEWORK = {
--- webapp/frontend/src/App.js
+++ webapp/frontend/src/App.js
@@ -1,52 +1,15 @@
 import React, { Component } from 'react';
-import { Table } from 'react-bootstrap';
 import Header from './components/Header';
 import SideNav from './components/SideNav';
+import Statistic from './components/Statistic';
 class App extends Component {
-  state = {
-    web_app: []
-  };
-  componentDidMount() {
-    fetch('http://127.0.0.1:8000/api/contain/')
-      .then(res => res.json())
-      .then((data) => {
-        this.setState({ web_app: data })
-      })
-      .catch(console.log)
-  }
-
   render() {
     return (
       <div className="App" align="center">
         <Header />
         <SideNav />
         <div className="main">
-        <Table striped bordered hover variant="dark">
-          <thead className="thead-dark">
-            <tr>
-              <th style={{ textAlign: 'center' }}>ContainID</th>
-              <th style={{ textAlign: 'center' }}>GarbageID</th>
-              <th style={{ textAlign: 'center' }}>BinID</th>
-              <th style={{ textAlign: 'center' }}>Amount</th>
-              <th style={{ textAlign: 'center' }}>Level</th>
-              <th style={{ textAlign: 'center' }}>DateTime</th>
-            </tr>
-          </thead>
-          <tbody className="align-items-center">
-            {
-              this.state.web_app.map(item => (
-              <tr key={item.contain_id}>
-                <td style={{ textAlign: 'center' }}>{item.contain_id}</td>
-                <td style={{ textAlign: 'center' }}>{item.garbage_id}</td>
-                <td style={{ textAlign: 'center' }}>{item.bin_id}</td>
-                <td style={{ textAlign: 'center' }}>{item.amount}</td>
-                <td style={{ textAlign: 'center' }}>{item.level}</td>
-                <td style={{ textAlign: 'center' }}>{item.date_time_value}</td>
-              </tr>
-              ))
-            }
-          </tbody>
-        </Table>
+          <Statistic />
         </div>
       </div>
     );

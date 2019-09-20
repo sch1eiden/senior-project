1f98550801b067b6e4d36ba4ab971d1c7aaacb06
Narongrit Rodphroboon <joker10130@gmail.com>
2019-09-20 10:38:45 +0700

    Add more layout in dashboard

--- webapp/frontend/src/pages/Dashboard.js
+++ webapp/frontend/src/pages/Dashboard.js
@@ -19,36 +19,60 @@ class Dashboard extends Component {
                 <div className="row">
                     <div className="col-6">
                         <div className="card">
-                        Percentage
+                            <div className="card-header">
+                                Percentage
+                            </div>
+                            <div className="card-body">
+                                Bin visual
+                            </div>
                         </div>
                     </div>
                     <div className="col-6">
-                        <Table striped bordered hover variant="dark">
-                            <thead className="thead-dark">
-                                <tr>
-                                <th style={{ textAlign: 'center' }}>ContainID</th>
-                                <th style={{ textAlign: 'center' }}>GarbageID</th>
-                                <th style={{ textAlign: 'center' }}>BinID</th>
-                                <th style={{ textAlign: 'center' }}>Amount</th>
-                                <th style={{ textAlign: 'center' }}>Level</th>
-                                <th style={{ textAlign: 'center' }}>DateTime</th>
-                                </tr>
-                            </thead>
-                            <tbody className="align-items-center">
-                                {
-                                this.state.web_app.map(item => (
-                                <tr key={item.contain_id}>
-                                    <td style={{ textAlign: 'center' }}>{item.contain_id}</td>
-                                    <td style={{ textAlign: 'center' }}>{item.garbage_id}</td>
-                                    <td style={{ textAlign: 'center' }}>{item.bin_id}</td>
-                                    <td style={{ textAlign: 'center' }}>{item.amount}</td>
-                                    <td style={{ textAlign: 'center' }}>{item.level}</td>
-                                    <td style={{ textAlign: 'center' }}>{item.date_time_value}</td>
-                                </tr>
-                                ))
-                                }
-                            </tbody>
-                        </Table>
+                        <div className="card">
+                            <div className="card-header">
+                                Statistic
+                            </div>
+                            <div className="card-body">
+                                <Table striped bordered hover variant="dark">
+                                    <thead className="thead-dark">
+                                        <tr>
+                                        <th style={{ textAlign: 'center' }}>ContainID</th>
+                                        <th style={{ textAlign: 'center' }}>GarbageID</th>
+                                        <th style={{ textAlign: 'center' }}>BinID</th>
+                                        <th style={{ textAlign: 'center' }}>Amount</th>
+                                        <th style={{ textAlign: 'center' }}>Level</th>
+                                        <th style={{ textAlign: 'center' }}>DateTime</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody className="align-items-center">
+                                        {
+                                        this.state.web_app.map(item => (
+                                        <tr key={item.contain_id}>
+                                            <td style={{ textAlign: 'center' }}>{item.contain_id}</td>
+                                            <td style={{ textAlign: 'center' }}>{item.garbage_id}</td>
+                                            <td style={{ textAlign: 'center' }}>{item.bin_id}</td>
+                                            <td style={{ textAlign: 'center' }}>{item.amount}</td>
+                                            <td style={{ textAlign: 'center' }}>{item.level}</td>
+                                            <td style={{ textAlign: 'center' }}>{item.date_time_value}</td>
+                                        </tr>
+                                        ))
+                                        }
+                                    </tbody>
+                                </Table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col">
+                        <div className="card">
+                            <div className="card-header">
+                                Location
+                            </div>
+                            <div className="card-body">
+                                Map
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>

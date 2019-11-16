b23fedcb7631daaa21f92f4218cee7e95ecacae5
joker10130 <narongrit.rodphroboon@outlook.co.th>
2019-11-16 18:11:29 +0700

    add donut ranking page

--- webapp/ranking/src/App.js
+++ webapp/ranking/src/App.js
@@ -1,66 +1,12 @@
-import React, { useState, useEffect } from 'react';
-import { Pie } from 'react-chartjs-2';
-import moment from 'moment';
-import firebase from './firebase';
-import _ from 'lodash';
-// import firebase from './firebase';
-
-const db = firebase.firestore();
+import React from 'react';
+import DoughnutChart from './components/DoughnutChart';
 
 const App = () => {
-  const [rankPie, setRankPie] = useState([]);
-  const [data, setData] = useState([]);
-  useEffect(() => {
-    const unsubscribe = db.collection('ranks').onSnapshot(ss => {
-      let data = [];
-      ss.forEach(document => {
-        data.push({
-          "docId": document.id,
-          "id": document.data().id,
-          "date": moment(document.data().date.toDate()).format("DD/MM/YYYY"),
-        })
-      })
-      setData(data);
-      setRankPie({
-        
-      })
-    })
-    return () => {
-      unsubscribe();
-    }
-  }, [])
-  const options = {
-    responsive: true,
-    maintainAspectRatio: true,
-    aspectRatio: 2,
-    legend: {
-        display: true,
-    },
-    tooltips: {
-        enabled: true,
-    },
-    scales: {
-        xAxes:[{
-            ticks:{
-                beginAtZero:true
-            },
-        }],
-        yAxes:[{
-            display:true,
-            ticks: {
-                beginAtZero:true,
-                suggestedMax: 100,
-            }
-        }]
-    }
-}
-
+  
   return (
-    <div className="App">
-      <Pie 
-        data={rankPie}
-        options={options}
-      />
+    <div id="App">
+      <h1 className="display-4" align="center">Smart Bin Ranking</h1>
+      <DoughnutChart />
     </div>
   );
 }
